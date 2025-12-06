a237d238cf8b5854d035213f199b2d47649cfa2d
divesh62 <ddivesh62@gmail.com>
2025-12-06 08:32:40 +0530

    Modify code

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -61,7 +61,10 @@ if(authState.user){
                     <textarea name="" onChange={(e)=>setPostContant(e.target.value) } value={postContant} className={styles.textarea} id=""></textarea>
                     <label htmlFor="fileUpload">
                         <div className={styles.fab}>
-                            <i className="fa fa-plus" aria-hidden="true"></i>
+                            <svg width={30} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+
                         </div>
                          <input hidden onChange={(e)=>setFileContant(e.target.files[0])}  type="file" id='fileUpload'  />
 
