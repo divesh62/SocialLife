81fb8b8403a9e3846eced7201d3cf5b57bdae40f
divesh62 <ddivesh62@gmail.com>
2025-12-06 08:48:33 +0530

    updated code

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -65,7 +65,7 @@ if(authState.user){
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
 
-                        </div>add
+                        </div>
                          <input hidden onChange={(e)=>setFileContant(e.target.files[0])}  type="file" id='fileUpload'  />
 
                     </label>
